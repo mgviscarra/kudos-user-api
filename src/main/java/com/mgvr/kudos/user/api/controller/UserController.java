609c5e8ea0865559bca414e766307cf5ed2f59a0
Mauricio Viscarra <mgviscarra@gmail.com>
2019-09-19 22:14:05 -0400

    Refactoring

--- src/main/java/com/mgvr/kudos/user/api/controller/UserController.java
+++ src/main/java/com/mgvr/kudos/user/api/controller/UserController.java
@@ -4,47 +4,32 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mgvr.kudos.user.api.constants.*;
-import com.mgvr.kudos.user.api.model.Kudo;
-import com.monitorjbl.json.JsonResult;
-import com.monitorjbl.json.JsonView;
-import com.monitorjbl.json.Match;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import com.mgvr.kudos.user.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.mgvr.kudos.user.api.dao.UserDao;
 import com.mgvr.kudos.user.api.model.User;
 
 
 @RestController
 @RequestMapping(UserApiRoutes.BASE_ROUTE)
 public class UserController {
-	@Autowired
-	private UserDao dao;
 
-	@Autowired
-	private RabbitTemplate rabbitTemplate;
+    @Autowired
+	UserService service;
 
 	@PostMapping(UserApiRoutes.POST_USER)
-	public String saveUser(@RequestBody User user) {
-		dao.saveUser(user);
-		return ApiMessages.USER_CREATED;
+	public ResponseEntity<String> saveUser(@RequestBody User user) {
+		service.addUser(user);
+        return new ResponseEntity<>(ApiMessages.USER_CREATED, HttpStatus.OK);
 	}
 
 	@GetMapping(UserApiRoutes.GET_USERS)
 	public ResponseEntity<List<User>> getAllUsers() {
-		List<User> users = dao.getAllUsers();
-		JsonResult json = JsonResult.instance();
-        List<User> listUsers= json.use(JsonView.with(users)
-                .onClass(User.class, Match.match()
-                        .exclude(DbFields.REAL_NAME)
-                        .exclude(DbFields.EMAIL)
-                        .exclude(DbFields.KUDOS)
-                                )).returnValue();
+		List<User> listUsers = service.getUsers();
         return new ResponseEntity<>(listUsers, HttpStatus.OK);
 	}
 	
@@ -53,50 +38,27 @@ public class UserController {
         if (allParams.size() == 0 || allParams.size() > 1) {
             return ResponseEntity.badRequest().body(ApiMessages.GET_USER_REQUIRED_PARAMETERS);
         }
-        User user = null;
-        switch (allParams.entrySet().iterator().next().getKey().toString()) {
-            case DbFields.ID:
-                user = dao.getUser(Integer.parseInt(allParams.get(DbFields.ID)));
-                break;
-            case DbFields.REAL_NAME:
-                user = dao.getUserByRealName(allParams.get(DbFields.REAL_NAME));
-                break;
-            case DbFields.NICK_NAME:
-                user = dao.getUserByNickName(allParams.get(DbFields.NICK_NAME));
-                break;
-        }
+        String field = allParams.entrySet().iterator().next().getKey().toString();
+        String value = allParams.get(field);
+        User user= service.getUserByField(field,value);
         if(user==null){
             return ResponseEntity.notFound().build();
         }
-        String responseKudosUser=  (String)rabbitTemplate.convertSendAndReceive
-                (RabbitmqExchangeName.EXCHANGE_NAME, RabbitmqRoutingKeys.KUDO_RPC_GET_KUDO_FOR_USER_REQUEST, user);
-        ObjectMapper mapper = new ObjectMapper();
-        if(responseKudosUser!=null){
-            List<Kudo> kudoList = mapper.readValue(responseKudosUser, mapper.getTypeFactory().constructCollectionType(List.class, Kudo.class));
-            user.setKudos(kudoList);
-        }
         return new ResponseEntity<>(user, HttpStatus.OK);
-
     }
 
 	
 	@PutMapping(UserApiRoutes.PUT_USER)
 	public ResponseEntity<String> updateUser(@PathVariable String id, @RequestBody User user) {
-		user.setId(Integer.parseInt(id));
-		dao.updateUser(user);
+		service.updateUser(id, user);
         return new ResponseEntity<>(ApiMessages.USER_UPDATED, HttpStatus.OK);
 	}
 	
 	@DeleteMapping(UserApiRoutes.DELETE_USER)
 	public ResponseEntity<String> deleteUser(@PathVariable String id) {
-		User user = dao.getUser(Integer.parseInt(id));
-		String responseDeleteKudos = (String)rabbitTemplate.convertSendAndReceive
-				(RabbitmqExchangeName.EXCHANGE_NAME, RabbitmqRoutingKeys.KUDO_RPC_KUDO_DELETE_REQUEST, user);
-		if(responseDeleteKudos.equalsIgnoreCase(ApiMessages.KUDOS_DELETED)){
-			dao.deleteUser(user);
-            return new ResponseEntity<>(ApiMessages.USER_DELETED, HttpStatus.OK);
+		if(service.deleteUser(id)){
+			return new ResponseEntity<>(ApiMessages.USER_DELETED, HttpStatus.OK);
 		}
         return new ResponseEntity<>(ApiMessages.USER_NOT_DELETED, HttpStatus.CONFLICT);
 	}
-
 }
---
+++ src/main/java/com/mgvr/kudos/user/api/service/UserService.java
@@ -0,0 +1,89 @@
+package com.mgvr.kudos.user.api.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mgvr.kudos.user.api.constants.ApiMessages;
+import com.mgvr.kudos.user.api.constants.DbFields;
+import com.mgvr.kudos.user.api.constants.RabbitmqExchangeName;
+import com.mgvr.kudos.user.api.constants.RabbitmqRoutingKeys;
+import com.mgvr.kudos.user.api.dao.UserDao;
+import com.mgvr.kudos.user.api.model.Kudo;
+import com.mgvr.kudos.user.api.model.User;
+import com.monitorjbl.json.JsonResult;
+import com.monitorjbl.json.JsonView;
+import com.monitorjbl.json.Match;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserDao dao;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void addUser(User user){
+        dao.saveUser(user);
+    }
+
+    public List<User> getUsers(){
+        List<User> users = dao.getAllUsers();
+        JsonResult json = JsonResult.instance();
+        List<User> listUsers= json.use(JsonView.with(users)
+                .onClass(User.class, Match.match()
+                        .exclude(DbFields.REAL_NAME)
+                        .exclude(DbFields.EMAIL)
+                        .exclude(DbFields.KUDOS)
+                )).returnValue();
+        return listUsers;
+    }
+
+    public User getUserByField(String field, String value) throws IOException {
+        User user=null;
+        switch (field) {
+            case DbFields.ID:
+                user = dao.getUser(Integer.parseInt(value));
+                break;
+            case DbFields.REAL_NAME:
+                user = dao.getUserByRealName(value);
+                break;
+            case DbFields.NICK_NAME:
+                user = dao.getUserByNickName(value);
+                break;
+        }
+        if(user==null){
+            return user;
+        }
+        String responseKudosUser=  (String)rabbitTemplate.convertSendAndReceive
+                (RabbitmqExchangeName.EXCHANGE_NAME, RabbitmqRoutingKeys.KUDO_RPC_GET_KUDO_FOR_USER_REQUEST, user);
+        ObjectMapper mapper = new ObjectMapper();
+        if(responseKudosUser!=null){
+            List<Kudo> kudoList = mapper.readValue(responseKudosUser, mapper.getTypeFactory().constructCollectionType(List.class, Kudo.class));
+            user.setKudos(kudoList);
+        }
+        return  user;
+    }
+
+    public void updateUser(String id, User user){
+        user.setId(Integer.parseInt(id));
+        dao.updateUser(user);
+    }
+
+    public boolean deleteUser(String id){
+        User user = dao.getUser(Integer.parseInt(id));
+        String responseDeleteKudos = (String)rabbitTemplate.convertSendAndReceive
+                (RabbitmqExchangeName.EXCHANGE_NAME, RabbitmqRoutingKeys.KUDO_RPC_KUDO_DELETE_REQUEST, user);
+        if(responseDeleteKudos.equalsIgnoreCase(ApiMessages.KUDOS_DELETED)){
+            dao.deleteUser(user);
+            return true;
+        }
+        return false;
+    }
+}
